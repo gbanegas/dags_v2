419ea8cb3cce774b949f395c92010cc5f3a1b39b
Rich H <rhaeussl@mason.gmu.edu>
2018-11-04 19:41:06 -0500

    Finshed with first past clean of keygen

--- include/keygeneration.h
+++ include/keygeneration.h
@@ -35,11 +35,11 @@ extern void  key_gen(gf *v, gf *y, matrix *G);
 
 extern int build_dyadic_signature(gf *dyadic_signature);
 
-extern void build_support(gf *signature_h, gf *u, gf *v);
+extern void build_support(gf *signature_h, gf *u, gf *v, gf *elements);
 
 extern void build_cauchy_matrix(gf *u, gf *v, matrix *H_cauchy);
 
-extern void build_trapdoor(const matrix *H_cauchy, const gf *v, const gf *u,
+extern int build_trapdoor(const matrix *H_cauchy, const gf *v, const gf *u,
 		gf *y, matrix *H);
 
 extern void project_H_on_F_q(const matrix *H, matrix *Hbase);
